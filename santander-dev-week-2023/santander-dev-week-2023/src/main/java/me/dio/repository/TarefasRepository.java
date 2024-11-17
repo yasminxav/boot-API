dab1516714c9e77f8b717a0b0fdafd1f3bd9d101
Yasmin Gabriele Xavier <ygxavier.yx@gmail.com>
2024-11-17 20:58:01 -0300

    Correções

--- santander-dev-week-2023/santander-dev-week-2023/src/main/java/me/dio/repository/TarefasRepository.java
+++ santander-dev-week-2023/santander-dev-week-2023/src/main/java/me/dio/repository/TarefasRepository.java
@@ -1,6 +1,5 @@
 package me.dio.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import me.dio.model.Tarefa;
 
